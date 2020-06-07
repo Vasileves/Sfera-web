a3ef9819d286a399b28be6ae8ca1a8b04045cfc0
Vasileves <55274720+Vasileves@users.noreply.github.com>
2020-06-08 00:22:01 +0300

    Added menu animation

---
+++ js/ui.js
@@ -0,0 +1,8 @@
+function unwrapMenu() {
+  var x = document.getElementById("menu-wrapper");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
